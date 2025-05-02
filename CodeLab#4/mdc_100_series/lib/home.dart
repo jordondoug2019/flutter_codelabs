7886a8a6b0655df8f8e022040da49d080ec78096
Jordon Douglas <jordondouglas@Jordons-MacBook-Air.local>
2025-05-02 09:18:02 -0500

    added search/filter icon

--- CodeLab#4/mdc_100_series/lib/home.dart
+++ CodeLab#4/mdc_100_series/lib/home.dart
@@ -17,32 +17,53 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards (102)
-  // TODO: Add a variable for Category (104)
+  // Make a collection of cards (102)
+  // Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
-    // TODO: Return an AsymmetricView (104)
-    // TODO: Pass Category variable to AsymmetricView (104)
+    // Return an AsymmetricView (104)
+    // Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
+      // Add app bar (102)
       appBar: AppBar(
-        leading: IconButton(
-      icon: const Icon(
-        Icons.menu,
-        semanticLabel: 'menu',
-      ),
-      onPressed: () {
-        print('Menu button');
-      },
-    ),
-    // TODO: Add buttons and title (102)
-    title: const Text('SHRINE'),
-  ),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: () {
+              print('Menu button');
+            },
+          ),
+          // Add buttons and title (102)
+          title: const Text('SHRINE'),
+          actions: <Widget>[
+            //added search icon 
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {
+                print('Search button');
+              },
+            ),
+            //added filter icon
+            IconButton(
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+              onPressed: () {
+                print('Filter Button');
+              },
+            )
+          ]),
       // TODO: Add a grid view (102)
       body: const Center(
         child: Text('You did it!'),
       ),
-       resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: false,
       // TODO: Set resizeToAvoidBottomInset (101)
     );
   }
