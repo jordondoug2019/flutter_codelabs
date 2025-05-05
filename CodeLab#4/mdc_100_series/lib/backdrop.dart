f4f7e88efddf6123fb0768c5d9096725da030057
Jordon Douglas <jordondouglas@Jordons-MacBook-Air.local>
2025-05-05 12:55:19 -0500

    Added comments/ close the front layer after menu selection

--- CodeLab#4/mdc_100_series/lib/backdrop.dart
+++ CodeLab#4/mdc_100_series/lib/backdrop.dart
@@ -3,60 +3,87 @@ import 'package:shrine/colors.dart';
 
 import 'model/product.dart';
 
+//Backdrop appears behind all other content and components.
+//It is composed of 2 layers: -BackLayer(Displays actions and filters) -frontLayer(Displays content)
+//Use a Backdrop to display interactive information and actions, such as Navigation or content filters
 //Add Velocity Constant
+//Velocity represents the speed and direction of movement
+//typicaaly used in gesture-driven animations and scroll physics.
 const double _kFlingVelocity = 0.5;
 
 //created backdrop widget
+//Stateful Widget: Can change overt time (UI may update based on interaction or data changes.)
 class Backdrop extends StatefulWidget {
-  final Category currentCategory;
-  final Widget frontLayer;
-  final Widget backLayer;
-  final Widget frontTitle;
-  final Widget backTitle;
+  //Parameters passed into the widget
+  final Category currentCategory; //Controls which caregory of data is shown
+  final Widget
+      frontLayer; //Widget shown in the foreground (like a product list)
+  final Widget backLayer; //Widget in the background (like a category menu)
+  final Widget frontTitle; // Title displayed when front layer is visible
+  final Widget backTitle; //Title displayed when back layer is visible
 
+//The constructor: creates and initalize an objcct when its first created.
   const Backdrop({
+    //Const makes the widget instance immutavle and compile-time constant when possible
     required this.currentCategory,
     required this.frontLayer,
     required this.backLayer,
     required this.frontTitle,
     required this.backTitle,
     Key? key,
-  }) : super(key: key);
+  }) : super(
+            key:
+                key); //Passes the key to the superclass (StatefulWIdget), useful for widget identity
 
+//Ties the widget to a _BackdrpState class, where all the interactive logic will be implemented
+//like showing/hiding the front layer
   @override
   _BackdropState createState() => _BackdropState();
 }
 
-class _BackdropState extends State<Backdrop>
-    with SingleTickerProviderStateMixin {
-  final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
+class _BackdropState extends State<
+        Backdrop> //State object for the Backdrop widget (defined earlier)
+    with
+        SingleTickerProviderStateMixin {
+  //allows the class to act as a ticker provider for animations- specifically for the ANimation COntroller
+  final GlobalKey _backdropKey = GlobalKey(
+      debugLabel:
+          'Backdrop'); //Globalkey gives access to the widget tree for actions like measuring size or identifying widgets uniquely
 
   //Add AnimationController Widget
-  late AnimationController _controller;
+  late AnimationController
+      _controller; //Controls the animation of the front layer sliding up/down
 
   @override
   void initState() {
+    //Initializing the Animation
     super.initState();
     _controller = AnimationController(
       duration: const Duration(milliseconds: 1000),
-      value: 1.0,
-      vsync: this,
+      value: 1.0, //Animates a value from 0.0 to 1.0 (used in transitions)
+      vsync:
+          this, //Reduces unneccasry rendering using SingleTickerProviderStateMixin
     );
     _controller.addListener(() {
-      print("Animation Status: ${_controller.status}");
+      print(
+          "Animation Status: ${_controller.status}"); //logs the status of the animation when it changes
     });
   }
 
   //add functions to get and change front layer visibility
+  //Checks whether the front layer is currently visible or becoming visible.
   bool get _frontLayerVisible {
     final status = _controller.status;
     return status == AnimationStatus.completed ||
         status == AnimationStatus.forward;
   }
+//toggle layer visibility
 
   void _toggleBackdropLayerVisibility() {
     print("Backdrop visibility tiggled!");
     setState(() {
+      //Fling- simulates a physical spring-like motion, allowing an animation to smoothly continue from a defined velocity
+      //after the user initates a drag or fling gesture
       _controller.fling(
         velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity,
       );
@@ -64,6 +91,19 @@ class _BackdropState extends State<Backdrop>
   }
 
   // add override for didUpdateWidget
+  //will be called whenever the widget configuration changes function in _BackdropState
+  @override
+  void didUpdateWidget(Backdrop old) {
+    super.didUpdateWidget(old);
+
+    if (widget.currentCategory != old.currentCategory) {
+      _toggleBackdropLayerVisibility();
+    } else if (!_frontLayerVisible) {
+      _controller.fling(velocity: _kFlingVelocity);
+    }
+  }
+
+  //disposed the controler when the widget is removed from the tree to free memory
   @override
   void dispose() {
     _controller.dispose();
@@ -73,11 +113,15 @@ class _BackdropState extends State<Backdrop>
   //Build Stack Widget
 
   //Add BuildCOntext and BoxCOnstraints parameters to _buildStack
+  //LayoutBuilder calls this to dynamically lay out the widget tree based on screen size
+  //_BuildStack() is the UI Layers
   Widget _buildStack(BuildContext contect, BoxConstraints constraints) {
     const double layerTitleHeight = 48.0;
     final Size layerSize = constraints.biggest;
-    final double layerTop = layerSize.height - layerTitleHeight;
-    final double layerBottom = layerTop + layerSize.height;
+    final double layerTop = layerSize.height -
+        layerTitleHeight; //Define the start point for the animation
+    final double layerBottom =
+        layerTop + layerSize.height; //Define the end points for the animation
 
     // Debug print statements
     print("Layer Size: $layerSize");
@@ -85,6 +129,7 @@ class _BackdropState extends State<Backdrop>
 
     //Create A relativerectween animation
     Animation<RelativeRect> layerAnimation = RelativeRectTween(
+      //RelativeRectTween: Defines the slide animation
       begin: RelativeRect.fromLTRB(0.0, layerTop, 0.0, layerBottom),
       end: const RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
     ).animate(_controller.view);
@@ -100,15 +145,17 @@ class _BackdropState extends State<Backdrop>
       ),
       //add a positioned transition
       PositionedTransition(
+          //uses this to animate the front layer
           rect: layerAnimation,
           child: _FrontLayer(
             child: widget.frontLayer,
           )),
       //wrap front layer in _FrontLayer
-  
     ]);
   }
 
+//Main UI
+//Builds an Appbar w/a menu button that toggles the front layer
   @override
   Widget build(BuildContext context) {
     var appBar = AppBar(
@@ -140,14 +187,15 @@ class _BackdropState extends State<Backdrop>
         appBar: appBar,
         //return a layoutBuilderWidget
         body: LayoutBuilder(
-          builder:  _buildStack,
-          )
-        );
+          //used to layout the backdrop layers using _buildStack
+          builder: _buildStack,
+        ));
   }
 }
 
-//Add _frontLayer class
-
+//Add _frontLayer widget
+//Important for visual appearance and elevation
+//Used to add a cut in the upper left corner
 class _FrontLayer extends StatelessWidget {
   //add on tap callback
   const _FrontLayer({
@@ -157,6 +205,7 @@ class _FrontLayer extends StatelessWidget {
 
   final Widget child;
 
+//Uswed BeveledRectangleBorder and Expanded to stretch the child widget
   @override
   Widget build(BuildContext context) {
     return Material(
--- CodeLab#4/mdc_100_series/lib/category_menu_page.dart
+++ CodeLab#4/mdc_100_series/lib/category_menu_page.dart
@@ -3,22 +3,31 @@ import 'package:flutter/material.dart';
 import 'colors.dart';
 import 'model/product.dart';
 
+//Stateless widget- It doesnt change (Does not store mutable state)
 class CategoryMenuPage extends StatelessWidget {
-  final Category currentCategory;
+  final Category currentCategory; 
   final ValueChanged<Category> onCategoryTap;
-  final List<Category> _categories = Category.values;
+  final List<Category> _categories = Category.values; //Grabs all possible values from the Category enum so they can be displayed
 
   const CategoryMenuPage({
     Key? key,
-    required this.currentCategory,
-    required this.onCategoryTap,
+    required this.currentCategory, //The category currently selected
+    required this.onCategoryTap, //callback function triggered when a category is tapped 
   }) : super(key: key);
 
+//Builds the UI for each individual categogey item in the list 
+//checks if the category is the currently selected one
+//if selected, it shows the name in bold 
+//adds a line below (highlight)
+//if not selected, it shows the name in a faded color
   Widget _buildCategory(Category category, BuildContext context) {
+    //converts something like Category.accessories to "ACCESSORIES"
     final categoryString =
         category.toString().replaceAll("Category.", '').toUpperCase();
+
     final ThemeData theme = Theme.of(context);
 
+//GestureDetector is a widget that detects and respons to user gestures
     return GestureDetector(
       onTap: () => onCategoryTap(category),
       child: category == currentCategory
@@ -49,13 +58,17 @@ class CategoryMenuPage extends StatelessWidget {
             ),
     );
   }
-
+//Widget's main UI definition 
   @override
   Widget build(BuildContext context) {
+    //center the widget on the screen 
     return Center(
       child: Container(
+        //adds padding at the top 
         padding: const EdgeInsets.only(top: 40.0),
+        //set background color 
         color: kShrinePink100,
+        //Displays a scrollable vertical list of categories, built using _buildCategory()
         child: ListView(
           children: _categories
               .map((Category c) => _buildCategory(c, context))
